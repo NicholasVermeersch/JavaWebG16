1912d1111c385b8bd3fd7a6b85cf88a96a2ad89a
DylanVanPaemel <dylan.vanpaemel.w1044@student.hogent.be>
2017-05-20 19:30:52 +0200

    -

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package config;
 
+import java.util.Properties;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableWebSecurity
@@ -43,4 +53,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf();
         
     }
+    
+     @Bean
+   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+      em.setDataSource(dataSource());
+      em.setPackagesToScan(new String[] { "domain" });
+ 
+      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+      em.setJpaVendorAdapter(vendorAdapter);
+      em.setJpaProperties(additionalProperties());
+ 
+      return em;
+   }
+ 
+   @Bean
+   public DataSource dataSource(){
+      DriverManagerDataSource dataSource = new DriverManagerDataSource();
+      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+      dataSource.setUrl("jdbc:mysql://localhost:3306/javaweb");
+      dataSource.setUsername( "root" );
+      dataSource.setPassword( "root" );
+      return dataSource;
+   }
+ 
+   @Bean
+   public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+      JpaTransactionManager transactionManager = new JpaTransactionManager();
+      transactionManager.setEntityManagerFactory(emf);
+      return transactionManager;
+   }
+ 
+   @Bean
+   public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+      return new PersistenceExceptionTranslationPostProcessor();
+   }
+ 
+   Properties additionalProperties() {
+      Properties properties = new Properties();
+      properties.setProperty("hibernate.hbm2ddl.auto", "create");
+      //properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+      return properties;
+   }
 }
--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -42,7 +42,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/javaweb?useSSL=false");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/javaweb");
         driverManagerDataSource.setUsername("root");
         driverManagerDataSource.setPassword("root");
         return driverManagerDataSource;
